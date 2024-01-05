64ba8d8c8766ea7da0fc4c11b23cc82281299ec5
Sandip shah <49273206+sandippshah@users.noreply.github.com>
2024-01-06 00:25:05 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -25,7 +25,7 @@ class Var(object):
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1003337276").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
-    APP_NAME = pikashow-file2link-bot-2
+    APP_NAME = None 
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Shaho007'))
     if 'DYNO' in environ:
         ON_HEROKU = True
@@ -36,9 +36,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',True))
     if HAS_SSL:
-        URL = "https://{}/".format(FQDN)
+        URL = "https://pikashow-file2link-bot-9888ff2cf3ed.herokuapp.com/".format(FQDN)
     else:
-        URL = "http://{}/".format(FQDN)
+        URL = "https://pikashow-file2link-bot-9888ff2cf3ed.herokuapp.com/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://file2link:Surajrathod.878@cluster0.qsj0pe0.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Rx_Bots')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
