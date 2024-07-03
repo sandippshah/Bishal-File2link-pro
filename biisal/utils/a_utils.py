1d58bdc44a0a4ee714ad6611f09937361a752ba8
Sandip shah <49273206+sandippshah@users.noreply.github.com>
2024-07-03 22:43:52 +0530

    Add files via upload

--- biisal/utils/a_utils.py
+++ biisal/utils/a_utils.py
@@ -1,10 +1,64 @@
 import pytz, random, string  
-from datetime import date 
+from datetime import datetime, date, time, timedelta
 from biisal.vars import Var
 from shortzy import Shortzy
+from biisal.utils.database import Database
+from biisal.utils.database import db
+db = Database(Var.DATABASE_URL, Var.name)
+import logging
+from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 
+
+
+from imdb import Cinemagoer 
+import asyncio
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
+from pyrogram import enums
+from typing import Union
+import pytz
+import random 
+import re
+import os
+from datetime import datetime, date, time, timedelta
+import string
+from typing import List
+from bs4 import BeautifulSoup
+import requests
+import aiohttp
+from shortzy import Shortzy
+import http.client
+import json
+
+
+
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+BTN_URL_REGEX = re.compile(
+    r"(\[([^\[]+?)\]\((buttonurl|buttonalert):(?:/{0,2})(.+?)(:same)?\))"
+)
+
+
+
+BANNED = {}
+VERIFY = {}
 TOKENS = {}
 VERIFIED = {}
+
+
+class temp(object):
+    GETALL = {}
+    SHORT = {}
+    SETTINGS = {}
+    IMDB_CAP = {}
+    VERIFY = {}
+    TOKENS = {}
+    VERIFIED = {}
+
+
 
 async def get_verify_shorted_link(link):
     shortzy = Shortzy(api_key=Var.SHORT_API, base_site=Var.SHORT_URL)
@@ -32,27 +86,111 @@ async def get_token(bot, userid, link):
     shortened_verify_url = await get_verify_shorted_link(link)
     return str(shortened_verify_url)
 
+
+async def get_verify_status(userid):
+    status = temp.VERIFY.get(userid)
+    if not status:
+        status = await db.get_verified(userid)
+        temp.VERIFY[userid] = status
+    return status
+
+async def update_verify_status(userid, date_temp, time_temp):
+    status = await get_verify_status(userid)
+    status["date"] = date_temp
+    status["time"] = time_temp
+    temp.VERIFY[userid] = status
+    await db.update_verification(userid, date_temp, time_temp)
+    
+        
 async def verify_user(bot, userid, token):
-    user = await bot.get_users(userid)
-    if user.id in TOKENS:
-        TOKENS[user.id][token] = True
+    user = await bot.get_users(int(userid))
+    if not await db.is_user_exist(user.id):
+        await db.add_user(user.id, user.first_name)
+        await bot.send_message(
+            Var.NEW_USER_LOG,
+            f"**New User Joined:** \n\n__My new friend__ [{user.mention}](tg://user?id={user.id}) __started your bot!__\nBot : @{Var.BOT_USERNAME}"
+        )
+        
+        
+    TOKENS[user.id] = {token: True}
     tz = pytz.timezone('Asia/Kolkata')
-    today = date.today()
-    VERIFIED[user.id] = str(today)
+    date_var = datetime.now(tz)+timedelta(hours=12) #added verification expire Time. in Hour 12hours 
+    temp_time = date_var.strftime("%H:%M:%S")
+    date_var, time_var = str(date_var).split(" ")
+    await update_verify_status(user.id, date_var, temp_time)    
     
-    
-    
+                
 async def check_verification(bot, userid):
-    user = await bot.get_users(userid)
+    user = await bot.get_users(int(userid))
+    if not await db.is_user_exist(user.id):
+        await db.add_user(user.id, user.first_name)
+        await bot.send_message(
+            Var.NEW_USER_LOG,
+            f"**New User Joined:** \n\n__My new friend__ [{user.mention}](tg://user?id={user.id}) __started your bot!__\nBot : @{Var.BOT_USERNAME}"
+        )
+        # premium verification bypass added 
+    if await db.has_premium_access(user.id):
+        return True      
+    else:
+        pass
+    if user.id in Var.ADMINS:
+        return True
+    else:
+        pass       
     tz = pytz.timezone('Asia/Kolkata')
     today = date.today()
-    if user.id in VERIFIED.keys():
-        EXP = VERIFIED[user.id]
-        years, month, day = EXP.split('-')
-        comp = date(int(years), int(month), int(day))
-        if comp<today:
-            return False
-        else:
-            return True
-    else:
+    now = datetime.now(tz)
+    curr_time = now.strftime("%H:%M:%S")
+    hour1, minute1, second1 = curr_time.split(":")
+    curr_time = time(int(hour1), int(minute1), int(second1))
+    status = await get_verify_status(user.id)
+    date_var = status["date"]
+    time_var = status["time"]
+    years, month, day = date_var.split('-')
+    comp_date = date(int(years), int(month), int(day))
+    hour, minute, second = time_var.split(":")
+    comp_time = time(int(hour), int(minute), int(second))
+    if comp_date<today:
         return False
+    else:
+        if comp_date == today:
+            if comp_time<curr_time:
+                return False
+            else:
+                return True
+        else:
+            return True                                        
+            
+async def get_seconds(time_string):
+    def extract_value_and_unit(ts):
+        value = ""
+        unit = ""
+
+        index = 0
+        while index < len(ts) and ts[index].isdigit():
+            value += ts[index]
+            index += 1
+
+        unit = ts[index:].lstrip()
+
+        if value:
+            value = int(value)
+
+        return value, unit
+
+    value, unit = extract_value_and_unit(time_string)
+
+    if unit == 's':
+        return value
+    elif unit == 'min':
+        return value * 60
+    elif unit == 'hour':
+        return value * 3600
+    elif unit == 'day':
+        return value * 86400
+    elif unit == 'month':
+        return value * 86400 * 30
+    elif unit == 'year':
+        return value * 86400 * 365
+    else:
+        return 0                
--- biisal/utils/database.py
+++ biisal/utils/database.py
@@ -2,6 +2,14 @@
 #(c) Adarsh-Goel
 import datetime
 import motor.motor_asyncio
+import datetime
+import pytz
+import time
+import re
+from pymongo.errors import DuplicateKeyError
+import motor.motor_asyncio
+from pymongo import MongoClient
+from biisal.vars import Var
 
 
 class Database:
@@ -10,13 +18,35 @@ class Database:
         self.db = self._client[database_name]
         self.col = self.db.users
         self.bannedList = self.db.bannedList
+        self.users = self.db.uersz
+        self.req = self.db.requests
 
     def new_user(self, id):
         return dict(
             id=id,
             join_date=datetime.date.today().isoformat()
         )
+    
+    
+    async def update_verification(self, id, date, time):
+        status = {
+            'date': str(date),
+            'time': str(time)
+        }
+        await self.col.update_one({'id': int(id)}, {'$set': {'verification_status': status}})
 
+    async def get_verified(self, id):
+        default = {
+            'date': "1999-12-31",
+            'time': "23:59:59"
+        }
+        user = await self.col.find_one({'id': int(id)})
+        if user:
+            return user.get("verification_status", default)
+        return default
+        
+        
+        
     async def add_user(self, id):
         user = self.new_user(id)
         await self.col.insert_one(user)
@@ -68,5 +98,82 @@ class Database:
             print(e)
             return e
     
-
     
+    # premium membership features 
+    
+    async def get_user(self, user_id):
+        user_data = await self.users.find_one({"id": user_id})
+        return user_data
+        
+    async def update_user(self, user_data):
+        await self.users.update_one({"id": user_data["id"]}, {"$set": user_data}, upsert=True)
+        
+
+    async def has_premium_access(self, user_id):
+        user_data = await self.get_user(user_id)
+        if user_data:
+            expiry_time = user_data.get("expiry_time")
+            if expiry_time is None:
+                # User previously used the free trial, but it has ended.
+                return False
+            elif isinstance(expiry_time, datetime.datetime) and datetime.datetime.now() <= expiry_time:
+                return True
+            else:
+                await self.users.update_one({"id": user_id}, {"$set": {"expiry_time": None}})
+        return False
+        
+    async def update_user(self, user_data):
+        await self.users.update_one({"id": user_data["id"]}, {"$set": user_data}, upsert=True)
+
+    async def update_one(self, filter_query, update_data):
+        try:
+            # Assuming self.client and self.users are set up properly
+            result = await self.users.update_one(filter_query, update_data)
+            return result.matched_count == 1
+        except Exception as e:
+            print(f"Error updating document: {e}")
+            return False
+
+    async def get_expired(self, current_time):
+        expired_users = []
+        if data := self.users.find({"expiry_time": {"$lt": current_time}}):
+            async for user in data:
+                expired_users.append(user)
+        return expired_users
+
+    async def remove_premium_access(self, user_id):
+        return await self.update_one(
+            {"id": user_id}, {"$set": {"expiry_time": None}}
+        )
+
+    async def check_trial_status(self, user_id):
+        user_data = await self.get_user(user_id)
+        if user_data:
+            return user_data.get("has_free_trial", False)
+        return False
+
+    async def give_free_trial(self, user_id):
+        #await set_free_trial_status(user_id)
+        user_id = user_id
+        seconds = 5*60         
+        expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=seconds)
+        user_data = {"id": user_id, "expiry_time": expiry_time, "has_free_trial": True}
+        await self.users.update_one({"id": user_id}, {"$set": user_data}, upsert=True)    
+    
+    async def check_remaining_uasge(self, userid):
+        user_id = userid
+        user_data = await self.get_user(user_id)        
+        expiry_time = user_data.get("expiry_time")
+        # Calculate remaining time
+        remaining_time = expiry_time - datetime.datetime.now()
+        return remaining_time
+
+
+    async def all_premium_users(self):
+        count = await self.users.count_documents({
+        "expiry_time": {"$gt": datetime.datetime.now()}
+        })
+        return count
+        
+
+db = Database(Var.DATABASE_URL, Var.name)
