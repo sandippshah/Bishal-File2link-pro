6ec3bf502a12e755c7407588ba9d844ac3d60192
Sandip shah <49273206+sandippshah@users.noreply.github.com>
2024-07-04 16:23:22 +0530

    Add files via upload

--- biisal/vars.py
+++ biisal/vars.py
@@ -4,6 +4,7 @@ from os import getenv, environ
 from dotenv import load_dotenv
 
 
+
 load_dotenv()
 bot_name = "Pikashow-File2Link"
 bisal_channel = "https://telegram.me/pikashow_Movies_Update"
@@ -13,12 +14,12 @@ class Var(object):
     SHORT_API = environ.get("SHORT_API", "6ac07ac202019e09497a0daad5abe04e11a98ac2") # shortlink api
     SHORT_URL = environ.get("SHORT_URL", "publicearn.com") # shortlink domain without https://
     VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/how2dow/55") # how to open link 
-    BOT_USERNAME = environ.get("BOT_USERNAME", "TGMoveisRobot") # bot username without @
-    VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.    
+    BOT_USERNAME = environ.get("BOT_USERNAME", "Pikashow_File2Link_Bot") # bot username without @
+    VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital. 
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID', '904789'))
     API_HASH = str(getenv('API_HASH', '2262ef67ced426b9eea57867b11666a1'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN' , '6819565837:AAG_MLRvjhtdqPX__EwavORHGraJrcsmQiE'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN' , '6936102398:AAGOoB8iWsjBj7BRAhtGfidVRKbrusMHxno'))
     name = str(getenv('name', 'biisal'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
@@ -32,7 +33,7 @@ class Var(object):
     ADMINS = [int(x) for x in os.environ.get("ADMINS", "622730585 1003337276").split()]  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None 
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'SpShah878'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'spshah878'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
@@ -42,14 +43,15 @@ class Var(object):
     FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',True))
     if HAS_SSL:
-        URL = "http://3.27.120.76:8080/".format(FQDN)
+        URL = "https://pikashowfile2link-a44747e6d083.herokuapp.com/".format(FQDN)
     else:
-        URL = "http://3.27.120.76:8080/".format(FQDN)
+        URL = "https://pikashowfile2link-a44747e6d083.herokuapp.com/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://file2link:Surajrathod.878@cluster0.qsj0pe0.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Rx_Bots')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
     BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))   
-    BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.Pʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ @biisal_bot ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
+    BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.Pʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ @spshah878 ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
     PLANS = str(getenv('PLANS' , '👋 ʜᴇʏ\n<b>-: Priumum Plan Details :-</b>\n• Bronze - 15 Days | 20₹ low budget plans\n• Silver - 1 Month | ₹40\n• Gold - 2 Month | ₹80\n• Platinum - 4 Month | ₹140 (Recommended)\n• Diamond 💎 - 6 Month | ₹200\n\n➣  UPI ID :  shah.910@paytm\n📸 ǫʀ ᴄᴏᴅᴇ - https://graph.org/file/42addd2d97784d0f9c9a6.jpg \n➣  Must Send a Screenshot '))
     
+
 
